e63eb9e7daf95a6b1369121cc1678f16c884e71e
solveig-linnea <42567747+solveig-linnea@users.noreply.github.com>
2020-02-10 20:09:01 +0100

    Prøve Oktober

---
+++ proveIT2Oktober/oppgave4/index.js
@@ -0,0 +1,7 @@
+//alert("Javascript er lastet");
+
+let far = prompt("Hva er temperaturen i farenheit?");
+let cel = (far-32)/1.8;
+
+alert(far + " grader i celsius er " + cel.toFixed(2) + " grader.")
+//Viser hvor mange grader et gitt antall grader av farenheit er i celsius.
